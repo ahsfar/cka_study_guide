b2df5aa99b3ed3f623d7e6ec7d1a2d354b7b08cb
ahsfar <ahsfar21@gmail.com>
2024-02-02 18:07:43 +0800

    added script for checking node

---
+++ scripts/check-nodes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Calculate the total number of nodes (subtracting 1 for the header row)
+total=$(kubectl get nodes | wc -l)
+total=$((total - 1))
+
+# Count how many nodes are in "Ready" status
+ready=$(kubectl get nodes | grep ' Ready' | wc -l)
+
+# Count how many nodes are not in "Ready" status (also subtracting 1 for the header row)
+notReady=$(kubectl get nodes | grep -v ' Ready' | wc -l)
+notReady=$((notReady - 1))
+
+# Display the counts
+echo "Total nodes: $total"
+echo "Ready nodes: $ready"
+echo "NotReady nodes: $notReady"
+
+
+# Also run this to make the script executable => chmod +x check_nodes.sh
